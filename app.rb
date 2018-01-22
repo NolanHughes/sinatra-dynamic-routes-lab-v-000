10b5860c265d847e2e7a0400bfcdc344a1e0a179
Nolan Hughes <nhughes987@gmail.com>
2018-01-22 01:58:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     binding.pry
     if @operation == "add"
-      @number1 + @number2
+      @"#{@number1 + @number2}"
     elsif @operation == "subtract"
       "#{@number1 - @number2}"
     elsif @operation == "multiply"
