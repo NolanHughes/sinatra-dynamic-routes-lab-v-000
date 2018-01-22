9df3ff1b263acbb5c73facb021709316821d4e99
Nolan Hughes <nhughes987@gmail.com>
2018-01-22 01:55:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,12 +38,12 @@ class App < Sinatra::Base
 
     if @operation = "add"
       "#{@number1 + @number2}"
-    elsif @operation = "add"
-      "#{@number1 + @number2}"
-    elsif @operation = "add"
-      "#{@number1 + @number2}"
-    elsif @operation = "add"
-      "#{@number1 + @number2}"
+    elsif @operation = "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation = "multiply"
+      "#{@number1 * @number2}"
+    elsif @operation = "divide"
+      "#{@number1 / @number2}"
     else
       "Not a valid operation! Try a different url"
     end
